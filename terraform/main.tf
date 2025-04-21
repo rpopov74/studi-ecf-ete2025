157d58ac2e791f470f131d9e35f0a5037f4628e4
rpopov74 <megevand.alexis@gmail.com>
2025-04-21 20:02:10 +0200

    Create EC2 Java

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,13 @@ provider "aws" {
 resource "aws_vpc" "yourmedia_vpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+#Create EC2
+resource "aws_instance" "java_ec2" {
+  ami           = "ami-04435e37b3df0b80b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Centos10_JDK21"
+  }
+}
