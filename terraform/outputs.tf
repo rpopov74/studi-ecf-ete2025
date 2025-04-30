6d4ec1f841c1376a8211ebaf274c5616e947bcf1
rpopov74 <megevand.alexis@gmail.com>
2025-04-30 21:54:09 +0200

    modifiy output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,7 @@
-# Output for EC2 public IP
-output "ec2_public_ip" {
-  value = aws_instance.java_ec2.public_ip
+# Output for VPC ID
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.ym_vpc.vpc_id
 }
 
 # Output for RDS endpoint
