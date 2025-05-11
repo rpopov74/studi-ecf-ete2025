32e48aa6054e19b921a0f2c2195360f4ee7107f3
rpopov74 <megevand.alexis@gmail.com>
2025-05-11 22:34:21 +0200

    spring boot hello world

---
+++ java/src/main/java/hello/world/studi/StudiApplication.java
@@ -0,0 +1,20 @@
+package hello.world.studi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController // Ajout de l'annotation pour exposer un endpoint web
+public class StudiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudiApplication.class, args);
+    }
+
+    @GetMapping("/") // Endpoint accessible à la racine
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
---
+++ java/src/test/java/hello/world/studi/StudiApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.world.studi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
